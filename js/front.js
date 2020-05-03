64d0fadaee9d26af32856ec61c16ed71e3fde404
recordboy <ksj_design@naver.com>
2020-05-03 16:04:10 +0900

    작업중

--- js/front.js
+++ js/front.js
@@ -10,118 +10,111 @@ var hand = null,
     handMoveSet = null,
     btnCoin = null,
     roulette = null,
-    btnResult = null,
+    btnControl = null,
     btnRock = null,
     btnPaper = null,
-
-
-    
-
     coinMoveSet = null,
+    msgArea = null,
     coinMoveNum = 0,
-    rouletteIng = false;
+    rouletteIng = false,
+    gameIng = false,
+    gameResultTxt = '',
+    rewardNum = 0;
 
     hand = document.getElementById('hand');
     btnCoin = document.getElementById('btnCoin');
     roulette = document.getElementById('roulette');
-    btnResult = document.getElementById('btnResult');
+    btnControl = document.getElementById('btnControl');
     btnRock = document.getElementById('btnRock');
     btnPaper = document.getElementById('btnPaper');
-
-
-
-
-    
-
-
+    msgArea = document.getElementById('msgArea');
     
 btnCoin.addEventListener('click', function() {
-    rouletteIng = true;
-    clearInterval(handMoveSet);
-    handMove(100);
+    if (!gameIng) {
+        gameIng = true;
+        handMove(50);
+        console.log('게임 시작')
+    }
 });
 
-btnResult.addEventListener('click', function(e) {
-
-    if (rouletteIng) {
-        
-        clearInterval(handMoveSet);
-
+btnControl.addEventListener('click', function(e) {
+    if (gameIng && rouletteIng) {
         if (e.target.id === 'btnScissors') {
+            console.log('가위를 냄');
             if (handNum === 0) {
-                clearInterval(handMoveSet);
-                setTimeout(function() {
-                    handMove(100);
-                }, 250);
-                console.log('비겼다');
+                gameResult('tie');
             } else if (handNum === 1) {
-                clearInterval(handMoveSet);
-                handMove(1000);
-                console.log('졌다');
+                gameResult('lose');
             } else if (handNum === 2) {
-                clearInterval(handMoveSet);
-                coinMove();
-                console.log('이겼다');
+                gameResult('win');
             }
         } else if (e.target.id === 'btnRock') {
+            console.log('바위를 냄');
             if (handNum === 0) {
-                clearInterval(handMoveSet);
-                coinMove();
-                console.log('이겼다');
+                gameResult('win');
             } else if (handNum === 1) {
-                clearInterval(handMoveSet);
-                setTimeout(function() {
-                    handMove(100);
-                    console.log('비겼다');
-                }, 250);
+                gameResult('tie');
             } else if (handNum === 2) {
-                clearInterval(handMoveSet);
-                handMove(1000);
-                console.log('졌다');
+                gameResult('lose');
             }
         } else if (e.target.id === 'btnPaper') {
+            console.log('보를 냄');
             if (handNum === 0) {
-                clearInterval(handMoveSet);
-                handMove(1000);
-                console.log('졌다');
+                gameResult('lose');
             } else if (handNum === 1) {
-                clearInterval(handMoveSet);
-                handMove(1000);
-                console.log('졌다');
+                gameResult('win');
             } else if (handNum === 2) {
-                clearInterval(handMoveSet);
-                setTimeout(function() {
-                    handMove(100);
-                }, 250);
-                console.log('비겼다');
+                gameResult('tie');
             }
-
         }
-
     }
-
-
-
-    
 });
 
-
-function handMove(time) {
-    handMoveSet = setInterval(function() {
-        handNum++;
-        if (handNum >= 3) {
-            handNum = 0;
-        }
-        if (handNum === 0) {
-            handShape(0, 1, 2);
-        } else if (handNum === 1) {
-            handShape(1, 2, 0);
-        } else if (handNum === 2) {
-            handShape(2, 0, 1);
-        }
-    }, time);
+function gameResult(res) {
+    gameResultTxt = res;
+    if (gameResultTxt ==='tie') {
+        clearInterval(handMoveSet);
+        rouletteIng = false;
+        msgResult(gameResultTxt);
+        console.log('비겼다');
+        setTimeout(function() {
+            handMove(50);
+        }, 350);
+    } else if (gameResultTxt ==='lose') {
+        clearInterval(handMoveSet);
+        rouletteIng = false;
+        gameIng = false;
+        msgResult(gameResultTxt);
+        console.log('졌다');
+    } else if (gameResultTxt ==='win') {
+        clearInterval(handMoveSet);
+        rouletteIng = false;
+        msgResult(gameResultTxt);
+        console.log('이겼다');
+        setTimeout(function() {
+            coinMove();
+        }, 350);
+    }
 }
 
+function handMove(time) {
+    if (!rouletteIng) {
+        rouletteIng = true;
+        handMoveSet = setInterval(function() {
+            handNum++;
+            if (handNum >= 3) {
+                handNum = 0;
+            }
+            if (handNum === 0) {
+                handShape(0, 1, 2);
+            } else if (handNum === 1) {
+                handShape(1, 2, 0);
+            } else if (handNum === 2) {
+                handShape(2, 0, 1);
+            }
+        }, time);
+    }
+}
 
 function handShape(res1, res2, res3) {
     hand.children[res1].classList.add('on');
@@ -131,22 +124,44 @@ function handShape(res1, res2, res3) {
 
 // 이겼을 때
 function coinMove() {
+    rewardNum = Math.floor((Math.random() * 12));
+    coinMoveNum = rewardNum;
     coinMoveSet = setInterval(function() {
         coinMoveNum++;
-        
         if (coinMoveNum >= roulette.children.length) {
             coinMoveNum = 0;
         }
-        console.log(coinMoveNum)
         roulette.children[coinMoveNum].classList.add('on');
         if (coinMoveNum === 0) {
             roulette.children[11].classList.remove('on');
         } else {
             roulette.children[coinMoveNum - 1].classList.remove('on');
-
         }
     }, 50);
-    
+    setTimeout(function() {
+        clearInterval(coinMoveSet)
+
+    }, 3000)
 }
-handMove(1000);
-// coinMove()
+
+
+function msgResult() {
+    if (gameResultTxt ==='tie') {
+        msgView(0, 1, 2);
+    } else if (gameResultTxt ==='lose') {
+        msgView(1, 2, 0);
+    } else if (gameResultTxt ==='win') {
+        msgView(2, 0, 1);
+    }
+}
+
+function msgView(res1, res2, res3) {
+    msgArea.children[res1].classList.add('on');
+    msgArea.children[res2].classList.remove('on');
+    msgArea.children[res3].classList.remove('on');
+    setTimeout(function() {
+        msgArea.children[res1].classList.remove('on');
+    }, 350)
+}
+
+
